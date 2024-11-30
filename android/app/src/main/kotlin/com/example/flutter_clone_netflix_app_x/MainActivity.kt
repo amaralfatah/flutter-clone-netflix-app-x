d2931090b923b69341047d46193913d057127e16
amaralfatah <amaralfatah.main@gmail.com>
2024-11-30 10:10:45 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_clone_netflix_app_x/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_clone_netflix_app_x
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class ColorPallete {
+  static const Color primaryColor = Color(0xFF0D47A1);
+  static const Color secondaryColor = Color(0xFF42A5F5);
+  static const Color accentColor = Color(0xFF64B5F6);
+  static const Color backgroundColor = Color(0xFFE3F2FD);
+  static const Color textColor = Color(0xFF000000);
+  static const Color errorColor = Color(0xFFD32F2F);
+  static const Color successColor = Color(0xFF388E3C);
+  static const Color warningColor = Color(0xFFFFA000);
+  static const Color infoColor = Color(0xFF1976D2);
+}
